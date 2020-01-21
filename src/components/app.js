6e82c27e7162394aed4341a69681e24c8df07e8f
marcelom97 <marcelomollaj@gmail.com>
2020-01-21 13:03:50 +0200

    [added] base text editor

---
+++ src/components/app.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-cobalt";
+
+export class app extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ""
+    };
+  }
+  onChange(newValue) {
+    console.log("change", newValue);
+    // this.setState({
+    //   data: newValue
+    // })
+  }
+  sendData() {
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div>
+        <AceEditor
+          mode="java"
+          theme="cobalt"
+          onChange={this.onChange.bind(this)}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{ $blockScrolling: true }}
+        />
+      </div>
+    );
+  }
+}
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import App from "./components/app";
+import { render } from "react-dom";
+
+const rootElement = document.getElementById("react-app");
+
+render(<App />, rootElement);
