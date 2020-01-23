720f0cfc666d74a11a6887bd79baa12ccf06c8c1
marcelom97 <marcelomollaj@gmail.com>
2020-01-23 16:20:01 +0200

    [added] jshint and trigger on button

--- src/components/app.js
+++ src/components/app.js
@@ -1,28 +1,44 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import AceEditor from "react-ace";
-import {Button, Col, Row} from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
+
+import { JSHINT } from "jshint";
 
 import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-terminal";
-import 'ace-builds/src-noconflict/ext-searchbox';
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-searchbox";
 
-export class app extends Component {
+class app extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: ""
     };
+    this.data = "";
   }
 
   onChange(newValue) {
-    console.log(newValue);
+    // console.log(newValue);
+    this.data = newValue;
     // this.setState(prevState => ({
     //   data: newValue
     // }))
   }
 
   sendData() {
+    var source = this.data;
+    var options = {
+      undef: true,
+      esversion: 7
+    };
+    var predef = {
+      foo: false
+    };
+
+    JSHINT(source, options, predef);
+
+    console.log(JSHINT.data());
   }
 
   render() {
@@ -31,17 +47,22 @@ export class app extends Component {
         <Col>
           <AceEditor
             mode="javascript"
-            theme="terminal"
+            theme="monokai"
             onChange={this.onChange.bind(this)}
             name="UNIQUE_ID_OF_DIV"
-            editorProps={{$blockScrolling: true}}
+            editorProps={{ $blockScrolling: true }}
           />
         </Col>
         <Col>
-          <Button variant={"outline-primary"} onClick={this.sendData.bind(this)} size={"sm"}>
+          <Button
+            variant={"outline-primary"}
+            onClick={this.sendData.bind(this)}
+            size={"sm"}
+          >
             Click
           </Button>
         </Col>
+        <Col>{}</Col>
       </Row>
     );
   }
